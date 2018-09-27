4ec42e6bc54904e58d028f60c163f72e40c6fe35
Peter Booker <mail@peterbooker.com>
2018-09-27 22:07:48 +0100

    remove unused enqueues

--- theme/functions.php
+++ theme/functions.php
@@ -94,12 +94,7 @@ add_action( 'widgets_init', 'pb_widgets_init' );
  */
 function pb_scripts() {
 	wp_enqueue_style( 'pb-style', get_stylesheet_uri() );
-
 	wp_enqueue_script( 'pb-script', get_template_directory_uri() . '/assets/js/theme.js', array( 'jquery' ), PB_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'pb_scripts' );
 
