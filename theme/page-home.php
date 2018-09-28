2572827f830b311f6e6001493bc08f5723224e8f
Peter Booker <mail@peterbooker.com>
2018-09-28 19:14:28 +0100

    Improve home page ready for deployment

--- theme/page-home.php
+++ theme/page-home.php
@@ -12,31 +12,64 @@ get_header();
 <section id="content">
 	<div class="panels">
 		<div class="title-panel">
-			<h1 class="title">Hi, my name is Peter.</h1>
-			<h4 class="sub-title">I solve problems faster than I create them.</h4>
+			<h1 class="title"><?php bloginfo( 'name' ); ?></h1>
+			<h4 class="sub-title"><?php bloginfo( 'description' ); ?></h4>
 		</div>
+
 		<div class="panel">
 			<div class="main">
-				<h2>Building Tools for the WordPress Project</h2>
-				<p>WPDirectory offers regex based text search of all 2+ million files in the WordPress Plugin and Theme Directories. It combines trigram indexing and concurrenct searching to deliver results in around 10 seconds.</p>
+				<h2>Searching all WordPress Plugin and Theme code</h2>
+				<p>I recently explored speeding up the process of downloading and regex searching all code in the WordPress Plugin and Theme Directories. The current methods use PHP scripts to download and extract every plugin and theme zip archive and then perform searches using tools like grep, ag, ripgrep, etc. This process could take the best part of a day.</p>
+				<p>Read about the journey, what worked, what didn't work and what I settled on.</p>
 				<a class="button" href="<?php echo esc_url( home_url( '/projects/wpdirectory/' ) ); ?>">Read More</a>
 			</div>
 			<div class="sub">
+				<div class="thumbnail">
+					<div class="post-thumbnail">
+						<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/feature-wpdirectory.png' ); ?>" alt="Placeholder Image" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php pb_recent_posts(); ?>
+
+		<div class="panel collapse blocks">
+			<div class="block">
+				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+			</div>
+			<div class="block">
+				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+			</div>
+			<div class="block">
+				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+			</div>
+			<div class="block">
+				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+			</div>
+			<div class="block">
+				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+			</div>
+			<div class="block">
 				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
 			</div>
 		</div>
+
 		<div class="panel">
-			<div class="sub">
-				<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
-			</div>
 			<div class="main">
 				<h2>I build things which save people time and help them reach their potential.</h2>
-				<p>From </p>
-				<a class="button" href="<?php echo esc_url( home_url( '/projects/wpdirectory/' ) ); ?>">My Work</a>
+				<p>My projects range from websites and services to REST APIs, micro services and CLI tools. I love identifying problems, researching solutions, prototyping ideas and developing solutions.</p>
+				<p>Take a look at my projects to see more.</p>
+				<a class="button" href="<?php echo esc_url( home_url( '/projects/' ) ); ?>">Projects</a>
+			</div>
+			<div class="sub">
+				<div class="thumbnail">
+					<div class="post-thumbnail">
+						<img src="http://via.placeholder.com/640x360" alt="Placeholder Image" />
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="panel">Three</div>
-		<div class="panel">Four</div>
 	</div><!-- .panels -->
 </section>
 <?php
