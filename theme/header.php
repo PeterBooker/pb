64a5c44e6a47f1771a60783c16d4235f3f0f0afb
Peter Booker <mail@peterbooker.com>
2018-09-30 22:26:54 +0100

    change theme-color meta to the dark grey, too much pink

--- theme/header.php
+++ theme/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<meta name="theme-color" content="#C3073F">
+	<meta name="theme-color" content="#1A1A1D">
 
 	<?php wp_head(); ?>
 </head>
