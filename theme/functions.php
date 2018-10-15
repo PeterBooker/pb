2de0659b6e10ecf1dd394de8abe753ce4da84901
Peter Booker <mail@peterbooker.com>
2018-10-15 15:32:25 +0100

    corrected function

--- theme/functions.php
+++ theme/functions.php
@@ -96,7 +96,7 @@ function pb_scripts() {
 	wp_enqueue_style( 'pb-style', get_stylesheet_uri(), array(), PB_VERSION );
 	wp_enqueue_script( 'pb-script', get_template_directory_uri() . '/assets/js/theme.js', array(), PB_VERSION, true );
 
-	if ( is_post( 'dotadata' ) ) {
+	if ( is_single( 'dotadata' ) ) {
 		wp_enqueue_style( 'dd-style', get_template_directory_uri() . '/assets/js/ddtips.latest.css', array(), PB_VERSION );
 		wp_enqueue_script( 'dd-script', get_template_directory_uri() . '/assets/js/ddtips.latest.js', array(), PB_VERSION, true );
 	}
